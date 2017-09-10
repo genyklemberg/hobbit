ee8fab2632db87fd578230fae87fd6049cb92124
Margarita Sadovets <margaritasadov@gmail.com>
2017-09-10 11:34:37 +0000

    Resolve "Picture download for avatar with custom filter"

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+  $key?: string,
+  email: string,
+  password: string,
+  birthday: Date,
+  name: string,
+  nick_name: string,
+  country: string,
+  location: string,
+  status: string,
+  gender: string,
+}
